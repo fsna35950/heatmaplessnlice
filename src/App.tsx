b800c4e44e08758f0d51d45af9330c2f74a66be1
fsna35950 <136624876+fsna35950@users.noreply.github.com>
2025-06-17 03:06:56 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/cnts/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -20,9 +20,9 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/blog" element={<BlogList />} />
+            <Route path="/blog/:postId" element={<BlogPost />} />
             <Route path="*" element={<NotFound />} />
-        <Route path="/blog" element={<BlogList />} />
-<Route path="/blog/:postId" element={<BlogPost />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
